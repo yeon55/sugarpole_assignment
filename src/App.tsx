8fc1620bb14c3058ff142b16b3f22f5ccf787af4
HAN SEOYEON <seoyeon@HANui-MacBookAir.local>
2024-03-16 13:53:39 +0900

    header 및 main 코드 및 스타일 수정작업 진행

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,89 @@
-import React from "react";
+import React, { useState } from "react";
 import "../public/index.css";
 
-const App = () => (
-  <>
-    <div className="min-h-screen flex justify-center items-center">
-      <h1 className="text-3xl font-bold text-blue-600">React</h1>
-    </div>
-  </>
-);
+const App = () => {
+  const [zoomFactor, setZoomFactor] = useState(1); // 초기 줌 배율은 1로 설정
+  const headerHeight = 116;
+
+  function handleZoom(): void {
+    setZoomFactor((prevZoomFactor) => prevZoomFactor + 0.1); // 10%씩 확대
+  }
+
+  function handleFlipH(): void {
+    //
+  }
+
+  function handleFlipV(): void {
+    //
+  }
+  function handleRotate(): void {
+    //
+  }
+  function handleInvert(): void {
+    //
+  }
+  function handleColormap(): void {
+    //
+  }
+  function handleReset(): void {
+    //
+  }
+  function handlePreviousImage(): void {
+    //
+  }
+  function handleNextImage(): void {
+    //
+  }
+
+  const mainStyle = {
+    height: `calc(100vh - ${headerHeight}px)`, // 헤더 높이만큼 뺀 나머지 공간을 main 영역의 높이로 설정
+  };
+
+  return (
+    <>
+      {/* header 부분 */}
+      <header className="min-w-max h-116 p-5 flex justify-center items-center relative z-10">
+        <div className=" text-xs flex items-center">
+          <div className="mr-4">Dicom Viewer(with Cornerstone.js)</div>
+          <ul className="inline-flex space-x-4 mx-4">
+            <li onClick={handleZoom}>Zoom</li>
+            <li onClick={handleFlipH}>Flip H</li>
+            <li onClick={handleFlipV}>Flip V</li>
+            <li onClick={handleRotate}>Rotate Delta 30</li>
+            <li onClick={handleInvert}>Invert</li>
+            <li onClick={handleColormap}>Apply Colormap</li>
+            <li onClick={handleReset}>Reset</li>
+          </ul>
+          <div className="ml-auto space-x-2">
+            <button
+              className="w-32 p-2.5 bg-button text-white"
+              onClick={handlePreviousImage}
+            >
+              Previous Image
+            </button>
+            <button
+              className="w-32 p-2.5 bg-button text-white"
+              onClick={handleNextImage}
+            >
+              Next Image
+            </button>
+          </div>
+        </div>
+      </header>
+
+      {/* main 부분 */}
+      <main className="relative z-0" style={mainStyle}>
+        <div className="w-full">
+          <img
+            className="w-full"
+            style={{ transform: `scale(${zoomFactor})` }}
+            src="/image/img0.png"
+            alt="img_main"
+          />
+        </div>
+      </main>
+    </>
+  );
+};
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,10 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    backgroundColor: (theme) => ({
+      ...theme("colors"),
+      button: "#0F62FE",
+    }),
   },
   plugins: [],
 };
