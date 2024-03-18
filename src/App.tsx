fc44c35cd89298c5386dfe680fc7a84b93d08a8c
HAN SEOYEON <seoyeon@HANui-MacBookAir.local>
2024-03-18 13:47:53 +0900

    두 개의 이미지 선택관련 코드 수정작업 진행중

--- src/App.tsx
+++ src/App.tsx
@@ -2,78 +2,92 @@ import React, { useState } from "react";
 import "../public/index.css";
 
 const App = () => {
-  const [zoomFactorImg0, setZoomFactorImg0] = useState(1); // 초기 줌 배율은 1로 설정
-  const [zoomedImg0, setZoomedImg0] = useState(false); // 확대 여부 상태
-  const [flippedImg0, setFlippedImg0] = useState(false); // 좌우 반전 여부 상태
-  const [flippedVerticallyImg0, setFlippedVerticallyImg0] = useState(false); // 상하 반전 여부 상태
-  const [rotationAngleImg0, setRotationAngleImg0] = useState(0); // 회전 각도 상태
+  const [images, setImages] = useState([
+    {
+      id: 0,
+      zoomFactor: 1,
+      zoomed: false,
+      flipped: false,
+      flippedVertically: false,
+      rotationAngle: 0,
+      src: "/image/img0.png",
+    },
+    {
+      id: 1,
+      zoomFactor: 1,
+      zoomed: false,
+      flipped: false,
+      flippedVertically: false,
+      rotationAngle: 0,
+      src: "/image/img1.png",
+    },
+  ]);
 
-  const [zoomFactorImg1, setZoomFactorImg1] = useState(1);
-  const [zoomedImg1, setZoomedImg1] = useState(false);
-  const [flippedImg1, setFlippedImg1] = useState(false);
-  const [flippedVerticallyImg1, setFlippedVerticallyImg1] = useState(false);
-  const [rotationAngleImg1, setRotationAngleImg1] = useState(0);
+  // 이미지 상태 업데이트 함수
+  const updateImageState = (
+    id: number,
+    newState: {
+      zoomFactor?: number;
+      zoomed?: boolean;
+      flipped?: boolean;
+      flippedVertically?: boolean;
+      rotationAngle?: number;
+    }
+  ) => {
+    setImages(
+      images.map((img) => (img.id === id ? { ...img, ...newState } : img))
+    );
+  };
+
   // Zoom 기능
-  function handleZoomImg0(): void {
-    if (!zoomedImg0) {
-      setZoomFactorImg0((prevZoomFactor) => prevZoomFactor + 0.05); // 5%씩 확대
-      setZoomedImg0(true); // 확대 상태 업데이트
-    }
-  }
-
-  function handleZoomImg1(): void {
-    if (!zoomedImg1) {
-      setZoomFactorImg1((prevZoomFactor) => prevZoomFactor + 0.05);
-      setZoomedImg1(true);
-    }
-  }
+  const handleZoom = (id: number) => {
+    updateImageState(id, {
+      zoomFactor: images[id].zoomed
+        ? images[id].zoomFactor
+        : images[id].zoomFactor + 0.05,
+      zoomed: true,
+    });
+  };
 
   // 좌우 반전 기능
-  function handleFlipHImg0(): void {
-    setFlippedImg0(!flippedImg0); // 좌우 반전 상태를 토글
-  }
-
-  function handleFlipHImg1(): void {
-    setFlippedImg1(!flippedImg1); // 좌우 반전 상태를 토글
-  }
+  const handleFlipH = (id: number) => {
+    updateImageState(id, { flipped: !images[id].flipped });
+  };
 
   // 상하 반전 기능
-  function handleFlipVImg0(): void {
-    setFlippedVerticallyImg0(!flippedVerticallyImg0); // 상하 반전 상태를 토글
-  }
-
-  function handleFlipVImg1(): void {
-    setFlippedVerticallyImg1(!flippedVerticallyImg1);
-  }
+  const handleFlipV = (id: number) => {
+    updateImageState(id, { flippedVertically: !images[id].flippedVertically });
+  };
 
   // 각도 조절 기능
-  function handleRotateImg0(): void {
-    setRotationAngleImg0((prevRotationAngle) => prevRotationAngle + 30); // 현재 회전 각도에 30도를 더하기
-  }
+  const handleRotate = (id: number) => {
+    updateImageState(id, { rotationAngle: images[id].rotationAngle + 30 });
+  };
 
-  function handleRotateImg1(): void {
-    setRotationAngleImg1((prevRotationAngle) => prevRotationAngle + 30);
-  }
+  // Invert 기능
+  const handleInvert = (id: number) => {
+    // Handle Invert
+  };
 
-  function handleInvert(): void {
-    //
-  }
+  // Colormap 적용 기능
+  const handleColormap = (id: number) => {
+    // Handle Colormap
+  };
 
-  function handleColormap(): void {
-    //
-  }
+  // Reset 기능
+  const handleReset = (id: number) => {
+    // Handle Reset
+  };
 
-  function handleReset(): void {
-    //
-  }
+  // Previous Image 기능
+  const handlePreviousImage = () => {
+    // Handle Previous Image
+  };
 
-  function handlePreviousImage(): void {
-    //
-  }
-
-  function handleNextImage(): void {
-    //
-  }
+  // Next Image 기능
+  const handleNextImage = () => {
+    // Handle Next Image
+  };
 
   return (
     <>
@@ -82,13 +96,13 @@ const App = () => {
         <div className=" text-xs flex items-center">
           <div className="mr-4">Dicom Viewer(with Cornerstone.js)</div>
           <ul className="inline-flex space-x-4 mx-4">
-            <li onClick={handleZoomImg0}>Zoom</li>
-            <li onClick={handleFlipHImg0}>Flip H</li>
-            <li onClick={handleFlipVImg0}>Flip V</li>
-            <li onClick={handleRotateImg0}>Rotate Delta 30</li>
-            <li onClick={handleInvert}>Invert</li>
-            <li onClick={handleColormap}>Apply Colormap</li>
-            <li onClick={handleReset}>Reset</li>
+            <li onClick={() => handleZoom(0)}>Zoom</li>
+            <li onClick={() => handleFlipH(0)}>Flip H</li>
+            <li onClick={() => handleFlipV(0)}>Flip V</li>
+            <li onClick={() => handleRotate(0)}>Rotate Delta 30</li>
+            <li onClick={() => handleInvert(0)}>Invert</li>
+            <li onClick={() => handleColormap(0)}>Apply Colormap</li>
+            <li onClick={() => handleReset(0)}>Reset</li>
           </ul>
           <div className="ml-auto space-x-2">
             <button
@@ -110,32 +124,22 @@ const App = () => {
       {/* main 부분 */}
       <main className="p-20 z-0 flex justify-center">
         <div className="relative w-full flex justify-between items-center">
-          <img
-            className="w-50%"
-            style={{
-              transform: `rotate(${rotationAngleImg0}deg) scaleX(${
-                flippedImg0 ? -1 : 1
-              }) scaleY(${
-                flippedVerticallyImg0 ? -1 : 1
-              }) scale(${zoomFactorImg0})`, // 회전 및 좌우 및 상하 반전 및 확대 적용
-            }}
-            src="/image/img0.png"
-            alt="img_0"
-            onClick={handleZoomImg0}
-          />
-          <img
-            className="w-50%"
-            style={{
-              transform: `rotate(${rotationAngleImg1}deg) scaleX(${
-                flippedImg1 ? -1 : 1
-              }) scaleY(${
-                flippedVerticallyImg1 ? -1 : 1
-              }) scale(${zoomFactorImg1})`,
-            }}
-            src="/image/img1.png"
-            alt="img_1"
-            onClick={handleZoomImg1}
-          />
+          {images.map((image) => (
+            <img
+              key={image.id}
+              className="w-50%"
+              style={{
+                transform: `rotate(${image.rotationAngle}deg) scaleX(${
+                  image.flipped ? -1 : 1
+                }) scaleY(${image.flippedVertically ? -1 : 1}) scale(${
+                  image.zoomFactor
+                })`,
+              }}
+              src={image.src}
+              alt={`img_${image.id}`}
+              onClick={() => handleZoom(image.id)}
+            />
+          ))}
         </div>
       </main>
     </>
